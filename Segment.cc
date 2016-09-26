fb3cde2b266cc38b0560d3e63594af51bd81bbc2
Sonny-Klotz <syk_778@hotmail.fr>
2016-09-26 19:47:25 +0200

    Init classes

--- Segment.cc
+++ Segment.cc
@@ -2,3 +2,22 @@ using namespace std;
 
 #include <iostream>
 
+class Point
+{
+	private:
+		double x;
+		double y;
+		
+	public:
+		
+}
+
+class Segment
+{
+	private:
+		Point p1;
+		Point p2;
+		
+	public:
+		
+}
