79c4ac88cec8929dfda9a0b43f17eb0ed37f2038
Utilisateur <syk_778@hotmail.fr>
2016-09-29 17:13:37 +0200

    dico exo 7 OK, a integrer avec CString

---
+++ dico.cc
@@ -0,0 +1,16 @@
+class Definition
+{
+	private:
+		CString Clef;
+		CString Def;
+	public:
+		Definition(char *c1, char *c2);
+		~Definition();
+		CString getClef();
+		CString getDef();
+};
+
+Definition::Definition(char *c1, char *c2): Clef(c1), Def(c2){}
+Definition::~Definition(){}
+CString Definition::getClef(){return Clef;}
+CString Definition::getDef(){return Def;}
