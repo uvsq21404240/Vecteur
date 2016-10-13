5b67ccd2b3e5b1392f52827c3d0991118de00394
Sonny Klotz <syk_778@hotmail.fr>
2016-10-13 21:37:41 +0200

    exo2

---
+++ td1/Echange.cc
@@ -0,0 +1,29 @@
+using namespace std;
+
+#include <iostream>
+
+void echange(int &a, int &b)
+{
+	int c = a; 
+	c = a;
+	a = b;
+	b = c;
+}
+
+/* 3 types de passage en argument aux fonctions
+ * valeur 		-> C C++ Java
+ * reference	-> C C++
+ * adresse		->   C++ Java (que les objets pour java) 
+ * */
+
+int main()
+{
+	int obj1 = 1;
+	int obj2 = 3;
+	
+	cout << obj1 << obj2 << "\n";
+	
+	echange(obj1, obj2);
+	
+	cout << obj1 << obj2 << "\n";
+}
